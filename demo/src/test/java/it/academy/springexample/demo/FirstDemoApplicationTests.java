82e0f804b104fcaf62648a6560041bef154422cf
Alexandru Chindris <alexandru.chindris.buisness@gmail.com>
2022-06-22 16:43:04 +0200

    Add files via upload

---
+++ demo/src/test/java/it/academy/springexample/demo/FirstDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.academy.springexample.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
